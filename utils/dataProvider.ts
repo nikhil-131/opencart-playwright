1e06814a8745d05ce058b056357457114c222320
Nikhil R <107304463+nikhil-131@users.noreply.github.com>
2026-08-21 22:15:58 +0530

    Update commented csv and json file reader menthods as it was causing failure in githib dataProvider.ts

--- utils/dataProvider.ts
+++ utils/dataProvider.ts
@@ -2,19 +2,20 @@ import fs from 'fs';
 import { parse } from 'csv-parse/sync';
 
 export class DataProvider{
+// Commenting this lines of code as it's causing workflow failure in github
+    
+// static getTestDataFromJson(filePath:string)
+// {
+//     let data:any =JSON.parse(fs.readFileSync(filePath, 'utf8'));
+//     return data;
+// }
 
-static getTestDataFromJson(filePath:string)
-{
-    let data:any =JSON.parse(fs.readFileSync(filePath, 'utf8'));
-    return data;
-}
 
-
-static getTestDataFromCsv(filePath:string)
-{
-     let data:any= parse(fs.readFileSync(filePath),{columns:true,skip_empty_lines:true})
-    return data;
-    }
+// static getTestDataFromCsv(filePath:string)
+// {
+//      let data:any= parse(fs.readFileSync(filePath),{columns:true,skip_empty_lines:true})
+//     return data;
+//     }
 
 
 }
